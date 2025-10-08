a65103b5c6675d50a51d9c08e2c61cd56aa26e86
JaimeBonBol <jaimebb2002@gmail.com>
2025-10-08 10:38:18 +0200

    Comienzo actividad 2 JavaScript

---
+++ UD1-JavaScript/Actividad2/ejercicio1/script.js
@@ -0,0 +1,18 @@
+function comprobarTexto() {
+
+    const numeros = ["0","1","2","3","4","5","6","7","8","9"];
+
+    // Elemento
+    const input = document.getElementById("txt");
+
+    // Valor del input
+    const valor = input.value;
+
+    for (let i = 0; i < valor.length; i++) {
+        if (!numeros.includes(valor[i])) {
+            input.value = "";
+            return;
+        }
+    }
+
+}
---
+++ UD1-JavaScript/Actividad2/ejercicio2/script.js
@@ -0,0 +1,13 @@
+function enfoque(id){
+    let input = document.getElementById(id);
+
+    input.style.backgroundColor = "gray";
+    input.style.color = "lightblue";
+}
+
+function desenfoque(id) {
+    let input = document.getElementById(id);
+
+    input.style.backgroundColor = "white";
+    input.style.color = "black";
+}
